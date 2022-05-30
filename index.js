f875282e3ef2f19b90499a184978ce941da8879c
DatNguyen <datnguyen@John.local>
2022-05-30 17:18:16 +0700

    Update command - Add counting user - Has join left and typing

--- index.js
+++ index.js
@@ -16,9 +16,35 @@ io.on('connection', (socket) => {
     io.emit('chat-message', msg);
   });
 
+  // Add sub message
+  socket.on('sub-chat-message', msg => {
+    io.emit('sub-chat-message', msg);
+  });
+
+  // Notify to others
   socket.on('notify-everyone', msg => {
     socket.broadcast.emit('notify-everyone', msg);
     // io.emit('notify-everyone', msg);
+  });
+
+  socket.on('disconnect', () => {
+    const mess = `A task has done. Remain tasks: ${io.engine.clientsCount}`;
+    io.emit('sub-chat-message', mess);
+  });
+
+  // Get current count
+  socket.on('status-count', () => {
+    const mess = `Current number of task: ${io.engine.clientsCount}`;
+    io.emit('sub-chat-message', mess);
+  });
+
+  // Typing and no longer typing
+  socket.on('typing', (user) => {
+    socket.broadcast.emit('typing', user);
+  });
+
+  socket.on('typing-no-longer', (user) => {
+    socket.broadcast.emit('typing-no-longer', user);
   });
 });
 
